08d5f542fe64a91117d20966aba2e480b3d8c484
MrAbhiX <99659192+MrAbhiX@users.noreply.github.com>
2022-02-19 22:29:35 +0530

    Update Clients.py

--- Script/assistant/TgCalls/Clients.py
+++ Script/assistant/TgCalls/Clients.py
@@ -1,13 +1,23 @@
 from pyrogram import Client
-from Script.Config import API_ID, API_HASH, BOT_TOKEN
+from Script.Config import API_ID, API_HASH, BOT_TOKEN, SESSION_NAME
 
 
-user = Client(
+bot = Client(
     "Indian Music Bot",
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="Script.Plugin"),
-)
+    )
+
+app = Client(
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_name=STRING1,
+    plugins=dict(root="Script.Plugins"),
+    )
+
+user = PyTgCalls(app)
 
 user.start()
+bot.run()
+idle()
