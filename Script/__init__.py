bd4973487f54790f69b7256bc8a823afd01eedaa
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-02-19 21:39:17 +0530

    Update __init__.py

--- Script/__init__.py
+++ Script/__init__.py
@@ -1,1 +1,18 @@
+import asyncio
+import os
+import time
+from os import listdir
+from os import mkdir
+import heroku3
+from git import Repo
+from rich.table import Table
+from rich.console import Console
+from aiohttp import ClientSession
+from motor.motor_asyncio import AsyncIOMotorClient as Bot
+from git.exc import GitCommandError, InvalidGitRepositoryError
 
+loop = asyncio.get_event_loop()
+vps = Console()
+
+UPDATE_BRANCH = "main"
+UPDATE_REPO = "https://github.com/TG-TeamIndia/IndianMusicBot"
