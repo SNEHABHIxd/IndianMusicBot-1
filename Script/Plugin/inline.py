9681b22ef203107359adaa47761689822fcb6eb3
MrAbhiX <99659192+MrAbhiX@users.noreply.github.com>
2022-02-22 14:02:52 +0530

    Update inline.py

--- Script/Plugin/inline.py
+++ Script/Plugin/inline.py
@@ -1,1 +1,18 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
+
+BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("⏸", callback_data="pause"),
+            InlineKeyboardButton("▶️", callback_data="resume"),
+            InlineKeyboardButton("⏭", callback_data="skip"),
+            InlineKeyboardButton("⏹", callback_data="stop"),
+            InlineKeyboardButton("🔇", callback_data="mute"),
+            InlineKeyboardButton("🔊", callback_data="unmute")
+        ],
+        [
+            InlineKeyboardButton("🗑 Close Menu", callback_data="close")
+        ]
+    ]
+)
