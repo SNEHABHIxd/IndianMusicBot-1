4d10ada159979591af7d6a592d45067911e0b484
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-13 03:51:50 +0530

    Update end.py

--- Script/Plugin/Admins/end.py
+++ Script/Plugin/Admins/end.py
@@ -1,7 +1,7 @@
 from Script.Plugin.queues import QUEUE, clear_queue
 import asyncio
 from Script.assistant.TgCalls.Clients import bot, user
-
+from pyrogram import filters
 from Script.Plugin.PyTgCalls import 
 
 from Script.Cache.admin_check import *
