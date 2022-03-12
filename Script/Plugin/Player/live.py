19d3275e10b57bac2b929910f07daad6b35f2bc1
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-13 04:35:31 +0530

    Update live.py

--- Script/Plugin/Player/live.py
+++ Script/Plugin/Player/live.py
@@ -1,1 +1,50 @@
 from pyrogram import filters
+
+from pytgcalls import StreamType
+
+from pytgcalls.types import AudioPiped, AudioVideoPiped
+
+import asyncio
+
+from Script.Cache.admin_check import *
+
+from Script.assistant.TgCalls.Clients import bot, user
+
+LIVE_CHATS = []
+
+@bot.on_message(filters.command(["stream", "vstream"]) & filters.group)
+@is_admin
+async def stream_func(_, message):
+    await message.delete()
+    state = message.command[0].lower()
+    try:
+        link = message.text.split(None, 1)[1]
+    except:
+        return await message.reply_text(f"<b>Usage:</b> <code>/{state} [link]</code>")
+    chat_id = message.chat.id
+    
+    if state == "stream":
+        damn = AudioPiped
+        emj = "🎵"
+    elif state == "vstream":
+        damn = AudioVideoPiped
+        emj = "🎬"
+    m = await message.reply_text("💫")
+    try:
+        if chat_id in QUEUE:
+            return await m.edit("❗️Please send <code>/stop</code> to end voice chat before live streaming.")
+        elif chat_id in LIVE_CHATS:
+            await user.change_stream(
+                chat_id,
+                damn(link)
+            )
+            await m.edit(f"{emj} Started streaming: [Link]({link})", disable_web_page_preview=True)
+        else:    
+            await user.join_group_call(
+                chat_id,
+                damn(link),
+                stream_type=StreamType().pulse_stream)
+            await m.edit(f"{emj} Started streaming: [Link]({link})", disable_web_page_preview=True)
+            LIVE_CHATS.append(chat_id)
+    except Exception as e:
+        return await m.edit(str(e))
