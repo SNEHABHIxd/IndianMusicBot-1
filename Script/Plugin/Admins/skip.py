32e2076a864519044a4e252367b79102a597a8c2
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-13 03:18:55 +0530

    Update skip.py

--- Script/Plugin/Admins/skip.py
+++ Script/Plugin/Admins/skip.py
@@ -1,1 +1,36 @@
 from Script.assistant.TgCalls.Clients import bot, user
+
+from Script.Cache.admin_check import *
+
+from Script.Plugin.PyTgCalls import skip_current_song
+from Script.Plugin.queues import QUEUE 
+
+
+@bot.on_message(filters.command("skip") & filters.group)
+@is_admin
+async def skip(_, message):
+    await message.delete()
+    chat_id = message.chat.id
+    if len(message.command) < 2:
+        op = await skip_current_song(chat_id)
+        if op == 0:
+            await message.reply_text("❗️Nothing in the queue to skip.")
+        elif op == 1:
+            await message.reply_text("❗️Empty queue, stopped streaming.")
+    else:
+        skip = message.text.split(None, 1)[1]
+        out = "🗑 <b>Removed the following song(s) from the queue:</b> \n"
+        if chat_id in QUEUE:
+            items = [int(x) for x in skip.split(" ") if x.isdigit()]
+            items.sort(reverse=True)
+            for x in items:
+                if x == 0:
+                    pass
+                else:
+                    hm = await skip_item(chat_id, x)
+                    if hm == 0:
+                        pass
+                    else:
+                        out = out + "\n" + f"<b>#️⃣ {x}</b> - {hm}"
+            await message.reply_text(out)
+
