27211dd977244f8bb47a61d516b06608e8fa45fd
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-13 03:33:28 +0530

    Update pause.py

--- Script/Plugin/Admins/pause.py
+++ Script/Plugin/Admins/pause.py
@@ -1,6 +1,22 @@
 from Script.assistant.TgCalls.Clients import bot, user
 
 import asyncio
+
 from Script.Cache.admin_check import *
 
 from Script.Plugin.queues import QUEUE
+
+
+@bot.on_message(filters.command("pause") & filters.group)
+@is_admin
+async def pause(_, message):
+    await message.delete()
+    chat_id = message.chat.id
+    if chat_id in QUEUE:
+        try:
+            await user.pause_stream(chat_id)
+            await message.reply_text("⏸ Paused streaming.")
+        except:
+            await message.reply_text("❗Nothing is playing.")
+    else:
+        await message.reply_text("❗Nothing is playing.")
