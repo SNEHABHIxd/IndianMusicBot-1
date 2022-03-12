80df13d8713ad5c746a72d4d2d7bb3d91dc20faa
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-13 03:40:32 +0530

    Update resume.py

--- Script/Plugin/Admins/resume.py
+++ Script/Plugin/Admins/resume.py
@@ -5,3 +5,18 @@ import asyncio
 from Script.Cache.admin_check import *
 
 from Script.Plugin.queues import QUEUE
+
+
+@bot.on_message(filters.command("resume") & filters.group)
+@is_admin
+async def resume(_, message):
+    await message.delete()
+    chat_id = message.chat.id
+    if chat_id in QUEUE:
+        try:
+            await app.resume_stream(chat_id)
+            await message.reply_text("⏸ Resumed streaming.")
+        except:
+            await message.reply_text("❗Nothing is playing.")
+    else:
+        await message.reply_text("❗Nothing is playing.")
