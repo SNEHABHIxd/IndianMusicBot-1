c16dec8942f43592a04b13ccd6c849fa83d0da2d
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-13 04:50:52 +0530

    TG-TeamIndia

--- Script/__main__.py
+++ Script/__main__.py
@@ -1,10 +1,10 @@
 # main Kang With Credit
+
 from Script.assistant.TgCalls.Clients import bot, user
+
 from pytgcalls import idle
 
-
+#credit de dena warna ma chod denge
 user.start()
-
 bot.run()
-
 idle()
