6801f18db1bdca83020ef89c72fde34173ad0bca
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-13 11:40:49 +0530

    Update inline.py

--- Script/Plugin/Helpers/inline.py
+++ Script/Plugin/Helpers/inline.py
@@ -2,10 +2,10 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQ
 
 
 BUTTONS = InlineKeyboardMarkup(
-    
+    [
         [
             InlineKeyboardButton("𝗦𝗨𝗣𝗣𝗢𝗥𝗧", url="t.me/indiansupportgroup"),
             InlineKeyboardButton("𝗖𝗛𝗔𝗡𝗡𝗘𝗟", url="t.me/indianupdatechannel")
                     ]
-    
+    ]
 )
