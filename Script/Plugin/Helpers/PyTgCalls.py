d5f6fe69a337fa0a8626f5ece7f90eb7bb39297f
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-19 23:30:43 +0530

    Update PyTgCalls.py

--- Script/Plugin/Helpers/PyTgCalls.py
+++ Script/Plugin/Helpers/PyTgCalls.py
@@ -51,8 +51,7 @@ async def skip_current_song(chat_id):
                 )
             pop_an_item(chat_id)
             await bot.send_photo(chat_id, photo = thumb,
-                                 caption = f"▶️ <b>Now playing:</b> [{title}]({link}) | `{type}` \n\n⏳ <b>Duration:</b> {duration}",
-                                 reply_markup = BUTTONS)
+                                 caption = f"▶️ <b>Now playing:</b> [{title}]({link}) | `{type}` \n\n⏳ <b>Duration:</b> {duration}")
             return [title, link, type, duration, thumb]
     else:
         return 0
