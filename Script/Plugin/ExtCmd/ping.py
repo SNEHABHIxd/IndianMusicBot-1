dad5cf7f1e4efb6261b0b9b4019bcc544027f47b
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-20 00:03:49 +0530

    Update ping.py

--- Script/Plugin/ExtCmd/ping.py
+++ Script/Plugin/ExtCmd/ping.py
@@ -1,1 +1,40 @@
+import os
+import sys
+import asyncio
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import Message
+from Script.assistant.TgCalls.Clients import bot
+from time import time
+from datetime import datetime
 
+# System Uptime
+START_TIME = datetime.utcnow()
+TIME_DURATION_UNITS = (
+    ('Week', 60 * 60 * 24 * 7),
+    ('Day', 60 * 60 * 24),
+    ('Hour', 60 * 60),
+    ('Min', 60),
+    ('Sec', 1)
+)
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
+
+@bot.on_message(filters.command(["ping", "uptime"]) & filters.group)
+async def ping(client, m: Message):
+   start = time()
+   current_time = datetime.utcnow()
+   m_reply = await m.reply_text("`...`")
+   delta_ping = time() - start
+   uptime_sec = (current_time - START_TIME).total_seconds()
+   uptime = await _human_time_duration(int(uptime_sec))
+   await m_reply.edit(f"`{delta_ping * 1000:.3f} ms` \n**Standtime 🔥** - `{uptime}`")
