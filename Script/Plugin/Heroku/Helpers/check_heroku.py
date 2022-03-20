98b4ce97bdc762e6ef10513e0523c36e0a2d1fc9
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-20 08:51:15 +0530

    Update check_heroku.py

--- Script/Plugin/Heroku/Helpers/check_heroku.py
+++ Script/Plugin/Heroku/Helpers/check_heroku.py
@@ -1,1 +1,31 @@
+#FOR USE THIS FEATURE U FILL HEROKU_APP_NAME AND HEROKU_API_KEY
 import os
+import re
+import heroku3
+from functools import wraps
+import asyncio
+from pyrogram import Client
+from pyrogram.types import Message
+from Script.Config import (
+    HEROKU_API_KEY,
+    HEROKU_APP_NAME,
+)
+
+
+def _check_heroku(func):
+    @wraps(func)
+    async def heroku_cli(client, message):
+        heroku_app = None
+        if not heroku_client:
+            await message.reply_text("`Please Add Heroku API Key To Use This Feature!`")
+        elif not HEROKU_APP_NAME:
+            await edit_or_reply(message, "`Please Add Heroku APP Name To Use This Feature!`")
+        if HEROKU_APP_NAME and heroku_client:
+            try:
+                heroku_app = heroku_client.app(HEROKU_APP_NAME)
+            except:
+                await message.reply_text(message, "`Heroku Api Key And App Name Doesn't Match! Check it again`")
+            if heroku_app:
+                await func(client, message, heroku_app)
+
+    return heroku_cli
