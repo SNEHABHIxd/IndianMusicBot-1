cb4218b08c02d882673bbe646f7df406cd1da54e
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-20 09:29:22 +0530

    Update set_var.py

--- Script/Plugin/Heroku/Vars/set_var.py
+++ Script/Plugin/Heroku/Vars/set_var.py
@@ -1,1 +1,26 @@
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from Script.Config import OWNER_ID
+from .Helpers.check_heroku import check_heroku
 
+
+@Client.on_message(command("setvar") & filters.user(OWNER_ID))
+@_check_heroku
+async def setvar(client: Client, message: Message, app_):
+    msg = await message.reply_text("`please wait...`")
+    heroku_var = app_.config()
+    _var = get_text(message)
+    if not _var:
+        await msg.edit("**usage:** `/setvar (var) (value)`")
+        return
+    if " " not in _var:
+        await msg.edit("**usage:** `/setvar (var) (value)`")
+        return
+    var_ = _var.split(" ", 1)
+    if len(var_) > 2:
+        await msg.edit("**usage:** `/setvar (var) (value)`")
+        return
+    _varname, _varvalue = var_
+    await msg.edit(f"**variable:** `{_varname}` \n**new value:** `{_varvalue}`")
+    heroku_var[_varname] = _varvalue
