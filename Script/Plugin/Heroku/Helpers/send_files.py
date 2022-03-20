40edfcc459e61a0cd7dd9e225c076f7b82b49419
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-20 08:56:08 +0530

    Update send_files.py

--- Script/Plugin/Heroku/Helpers/send_files.py
+++ Script/Plugin/Heroku/Helpers/send_files.py
@@ -1,1 +1,31 @@
+import os
+import re
+import heroku3
+from pyrogram import Client
+from pyrogram.types import Message
 
+
+
+async def edit_or_send_as_file(
+    text: str,
+    message: Message,
+    client: Client,
+    caption: str = "`Result!`",
+    file_name: str = "result",
+    parse_mode="md",
+):
+    """Send As File If Len Of Text Exceeds Tg Limit Else Edit Message"""
+    if not text:
+        await message.edit("`there is something other than text, aborting...`")
+        return
+    if len(text) <= 1024:
+        return await message.edit(text, parse_mode=parse_mode)
+
+    await message.edit("`output is too large, sending as file!`")
+    file_names = f"{file_name}.text"
+    open(file_names, "w").write(text)
+    await client.send_document(message.chat.id, file_names, caption=caption)
+    await message.delete()
+    if os.path.exists(file_names):
+        os.remove(file_names)
+    return
