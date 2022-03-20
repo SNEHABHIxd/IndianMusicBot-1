04bc13de6ebd8673f7e3b71cedfd8b388a782d38
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-20 09:08:22 +0530

    Update logs.py

--- Script/Plugin/Heroku/logs.py
+++ Script/Plugin/Heroku/logs.py
@@ -1,1 +1,17 @@
+from pyrogram import Client, filters
+from .Helpers.check_heroku import check_heroku
+from Script.Config import (
+    OWNER_ID,
+    HEROKU_APP_NAME,
+)
+from .Helpers.send_files import edit_or_send_as_file
 
+
+
+@Client.on_message(filters.command(["logs"]) & filters.user(OWNER_ID))
+@_check_heroku
+async def logs(client: Client, message: Message, happ):
+    msg = await message.reply_text("`please wait for a moment!`")
+    logs = happ.get_log()
+    capt = f"Heroku logs of `{HEROKU_APP_NAME}`"
+    await edit_or_send_as_file(logs, msg, client, capt, "logs")
