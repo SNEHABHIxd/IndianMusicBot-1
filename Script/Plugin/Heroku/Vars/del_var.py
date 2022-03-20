c04b17acecf1edb0cb0d30a167aba263d840cd2c
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-20 09:20:16 +0530

    Update del_var.py

--- Script/Plugin/Heroku/Vars/del_var.py
+++ Script/Plugin/Heroku/Vars/del_var.py
@@ -1,1 +1,19 @@
+from pyrogram import Client, filters
+from .Helpers.check_heroku import check_heroku
+import heroku3
 
+
+@Client.on_message(command("setvar") & filters.user(OWNER_ID))
+@_check_heroku
+async def delvar(client: Client, message: Message, app_):
+    msg = await message.reply_text("`please wait...!`")
+    heroku_var = app_.config()
+    _var = get_text(message)
+    if not _var:
+        await msg.edit("`give a var name to delete!`")
+        return
+    if _var not in heroku_var:
+        await msg.edit("`this var doesn't exists!`")
+        return
+    await msg.edit(f"sucessfully deleted var `{_var}`")
+    del heroku_var[_var]
