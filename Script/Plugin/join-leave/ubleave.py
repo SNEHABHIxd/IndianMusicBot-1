6d4ffdff44d40ef970868e9cfbe98fa05de465c6
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-20 13:01:30 +0530

    hehehe

---
+++ Script/Plugin/join-leave/ubleave.py
@@ -0,0 +1,17 @@
+from pyrogram import filters, Client
+from pyrogram.types import Message
+from Script.assistant.TgCalls.Clients import abhi
+from Script.Cache.admin_check import *
+import asyncio
+
+
+@Client.on_message(command(["userbotleave", "ubleave"]) & filters.group & ~filters.edited & filters.user(OWNER_ID))
+@is_admin
+async def rem(client, message):
+    try:
+        await abhi.send_sticker(message.chat.id, "CAACAgUAAx0CYPNCJwACA0RhbkLHaItFAAFFSUQZW3YhLiqJb2MAAgYFAAIclOFWYPPBpmhRMYUhBA")
+        await abhi.send_message(message.chat.id, "✅ I'm leaving your group, bye bye!")
+        await abhi.leave_chat(message.chat.id)
+    except:
+        await message.reply_text("<b>user couldn't leave your group, may be floodwaits.\n\nor manually kick me from your group</b>")
+        return
