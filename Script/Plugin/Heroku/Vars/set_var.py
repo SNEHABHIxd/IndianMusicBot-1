75c85caf090509b822daafad01d364338f76bab6
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-21 21:22:55 +0530

    Update set_var.py

--- Script/Plugin/Heroku/Vars/set_var.py
+++ Script/Plugin/Heroku/Vars/set_var.py
@@ -2,10 +2,10 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from Script.Config import OWNER_ID
-from Script.Plugin.Helpers.Heroku import check_heroku
+from Script.Plugin.Helpers.Heroku import _check_heroku
 
 
-@Client.on_message(command("setvar") & filters.user(OWNER_ID))
+@Client.on_message(filters.command("setvar") & filters.user(OWNER_ID))
 @_check_heroku
 async def setvar(client: Client, message: Message, app_):
     msg = await message.reply_text("`please wait...`")
