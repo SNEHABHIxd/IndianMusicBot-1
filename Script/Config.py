b33b6cc4dc2702217e5ace3bf6f9d07129a458bf
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-21 15:15:22 +0530

    Update Config.py

--- Script/Config.py
+++ Script/Config.py
@@ -6,7 +6,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = list(map(int, getenv("SUDO_USERS").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID").split()))
 
 #soon adding new plugins
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
