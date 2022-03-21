5f3b3261ad713bf67e1fa5f1909046c839acfbbe
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-21 13:20:24 +0530

    Update PyTgCalls.py

--- Script/Plugin/Helpers/Player/PyTgCalls.py
+++ Script/Plugin/Helpers/Player/PyTgCalls.py
@@ -1,6 +1,6 @@
 import asyncio
 from Script.assistant.TgCalls.Clients import bot, user
-from Script.Plugin.Helpers.queues import QUEUE, add_to_queue, get_queue, clear_queue, pop_an_item
+from Script.Plugin.Helpers.Player import QUEUE, add_to_queue, get_queue, clear_queue, pop_an_item
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
 from pytgcalls.types.input_stream.quality import (
     HighQualityAudio,
