1f57d0eeff407b202dd6891c39c7f0cd19b6a8a0
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-21 22:30:47 +0530

    Update Clients.py

--- Script/assistant/TgCalls/Clients.py
+++ Script/assistant/TgCalls/Clients.py
@@ -20,6 +20,6 @@ user = PyTgCalls(abhi)
 
 
 with Client("Indian Music Bot", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
-    indian = app.get_me()
+    me_bot = app.get_me()
 with abhi as app:
     me_abhi = app.get_me()
