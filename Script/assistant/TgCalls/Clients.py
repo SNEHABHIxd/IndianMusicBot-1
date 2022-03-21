0c17ad3dd211b997c737f5572f341d54f8f9e5f6
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-22 00:00:51 +0530

    Update Clients.py

--- Script/assistant/TgCalls/Clients.py
+++ Script/assistant/TgCalls/Clients.py
@@ -7,13 +7,14 @@ bot = Client(
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
+    plugins=dict(root="Script.Plugin"),
     )
 
 abhi = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     session_name=SESSION_NAME,
-    plugins=dict(root="Script.Plugin"),
+    
     )
 
 user = PyTgCalls(abhi)
