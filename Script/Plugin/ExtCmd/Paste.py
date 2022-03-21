99ddcaabd9d9c6b910ba97e144b1c438c420dacb
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-21 13:45:44 +0530

    Update Paste.py

--- Script/Plugin/ExtCmd/Paste.py
+++ Script/Plugin/ExtCmd/Paste.py
@@ -26,41 +26,42 @@ async def isPreviewUp(preview: str) -> bool:
         if status == 404 or (status == 200 and size == 0):
             await asyncio.sleep(0.4)
         else:
-            return status == 200
+            return True if status == 200 else False
     return False
 
 
-@Client.on_message(command(["paste"]))
+@Client.on_message(filters.command("paste") & ~filters.edited)
 async def paste_func(_, message):
-    if message.reply_to_message:
-        m = await message.reply_text("Pasting...")
-        if message.reply_to_message.text:
-            content = str(message.reply_to_message.text)
+    if not message.reply_to_message:
+        return await message.reply_text(
+            "Reply To A Message With /paste"
+        )
+    m = await message.reply_text("Pasting...")
+    if message.reply_to_message.text:
+        content = str(message.reply_to_message.text)
+    elif message.reply_to_message.document:
+        document = message.reply_to_message.document
+        if document.file_size > 1048576:
+            return await m.edit(
+                "You can only paste files smaller than 1MB."
+            )
+        if not pattern.search(document.mime_type):
+            return await m.edit("Only text files can be pasted.")
+        doc = await message.reply_to_message.download()
+        async with aiofiles.open(doc, mode="r") as f:
+            content = await f.read()
+        os.remove(doc)
+    link = await paste(content)
+    preview = link + "/preview.png"
+    button = InlineKeyboard(row_width=1)
+    button.add(InlineKeyboardButton(text="Paste Link", url=link))
 
-        elif message.reply_to_message.document:
-            document = message.reply_to_message.document
-            if document.file_size > 1048576:
-                return await m.edit(
-                    "You can only paste files smaller than 1MB."
-                )
-            if not pattern.search(document.mime_type):
-                return await m.edit("Only text files can be pasted.")
-            doc = await message.reply_to_message.download()
-            async with aiofiles.open(doc, mode="r") as f:
-                content = await f.read()
-            os.remove(doc)
-        link = await paste(content)
-        preview = link + "/preview.png"
-        button = InlineKeyboard(row_width=1)
-        button.add(InlineKeyboardButton(text="Paste Link", url=link))
-
-        if await isPreviewUp(preview):
+    if await isPreviewUp(preview):
+        try:
             await message.reply_photo(
                 photo=preview, quote=False, reply_markup=button
             )
             return await m.delete()
-        await m.edit(link)
-    else:
-        await message.reply_text("Reply To A Message With /paste")
-
-
+        except Exception:
+            pass
+    return await m.edit(link)
