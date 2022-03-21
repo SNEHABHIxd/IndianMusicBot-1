9dad0a95c61c6cd18067623ea3e38fd3c166271c
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-21 14:22:13 +0530

    Create send_msg.py

---
+++ Script/Plugin/Helpers/Broadcast/send_msg.py
@@ -0,0 +1,31 @@
+import asyncio
+import os
+from pyrogram.errors import (
+    FloodWait,
+    InputUserDeactivated,
+    PeerIdInvalid,
+    UserIsBlocked,
+)
+
+BROADCAST = bool(os.environ.get("BROADCAST_AS_COPY", ""))
+
+# IF YOU FILL BROADCAST VALUE TRUE THEN YOUR BOT COPY MSG AND SEND TO ALL GROUPS
+#IF YOU FILL BROADCAST VALUE FALSE THEN YOUR BOT FORWARD MSG TO ALL GROUPS
+async def send_msg(user_id, message):
+    try:
+        if BROADCAST is False:
+            await message.forward(chat_id=user_id)
+        elif BROADCAST is True:
+            await message.copy(chat_id=user_id)
+        return 200, None
+    except FloodWait as e:
+        await asyncio.sleep(int(e.x))
+        return send_msg(user_id, message)
+    except InputUserDeactivated:
+        return 400, f"{user_id} : deactivated\n"
+    except UserIsBlocked:
+        return 400, f"{user_id} : blocked the bot\n"
+    except PeerIdInvalid:
+        return 400, f"{user_id} : user id invalid\n"
+    except Exception:
+        return 500, f"{user_id} : {traceback.format_exc()}\n"
