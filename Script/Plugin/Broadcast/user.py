279d20ed3c3aa1fe56f30dd9d94ddea8cc967281
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-03-21 14:58:57 +0530

    Update user.py

--- Script/Plugin/Broadcast/user.py
+++ Script/Plugin/Broadcast/user.py
@@ -1,1 +1,28 @@
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Dialog, Chat, Message
+from pyrogram.errors import UserAlreadyParticipant
+from Script import abhi
+from Script.Config import OWNER_ID
 
+@Client.on_message(filters.command(["assbroadcast"]) & filters.user(OWNER_ID))
+async def broadcast(_, message: Message):
+    sent=0
+    failed=0
+    if message.from_user.id not in OWNER_ID:
+        return
+    else:
+        wtf = await message.reply("' broadcast started..`")
+        if not message.reply_to_message:
+            await wtf.edit("**__Ƥɭɘɑsɘ Ʀɘƥɭy Ƭø ɑ Mɘssɑʛɘ Ƭø Stɑɤt Ɓɤøɑɗƈɑst ...__**")
+            return
+        lmao = message.reply_to_message.text
+        async for dialog in aditya.iter_dialogs():
+            try:
+                await abhi.send_message(dialog.chat.id, lmao)
+                sent = sent+1
+                await wtf.edit(f"`Broadcasting` \n\n**Total success:** `{sent}` chats \n**Total failed:** {failed} chats")
+                await asyncio.sleep(3)
+            except:
+                failed=failed+1
+        await message.reply_text(f"`Broadcasting completed!` \n\n**Total done:** `{sent}` chats \n**Total failed:** {failed} chats")
